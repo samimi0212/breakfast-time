ff7df915c59b89bd56598fd185e68d3182adbcaf
samimi0212 <71037945+samimi0212@users.noreply.github.com>
2026-03-15 17:53:38 +0100

    Create MenuSection.tsx

---
+++ src/components/MenuSection.tsx
@@ -0,0 +1,31 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import HowItWorks from "@/components/HowItWorks";
+import MenuHighlight from "@/components/MenuHighlight";
+import MenuSection from "@/components/MenuSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
+import Testimonials from "@/components/Testimonials";
+import AboutSection from "@/components/AboutSection";
+import DeliveryZone from "@/components/DeliveryZone";
+import FinalCTA from "@/components/FinalCTA";
+import Footer from "@/components/Footer";
+
+const Index = () => (
+  <>
+    <Navbar />
+    <main>
+      <HeroSection />
+      <MenuHighlight />
+      <MenuSection />
+      <HowItWorks />
+      <WhyChooseUs />
+      <Testimonials />
+      <AboutSection />
+      <DeliveryZone />
+      <FinalCTA />
+    </main>
+    <Footer />
+  </>
+);
+
+export default Index;
